403f2c0bb8f241564c35ca4e63fad325b839728b
Guyep <44256059+Guyep@users.noreply.github.com>
2023-11-03 23:42:45 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,114 +1,37 @@
-require('dotenv').config();
-const crypto = require('crypto');
-const express = require("express");
-const path = require("path");
-const jwt = require("jsonwebtoken");
-const fetch = require('node-fetch');
-const cookieParser = require('cookie-parser');
-const WEBCHAT_SECRET = process.env.WEBCHAT_SECRET;
-const DIRECTLINE_ENDPOINT_URI = process.env.DIRECTLINE_ENDPOINT_URI;
-const APP_SECRET = process.env.APP_SECRET;
-const directLineTokenEp = `https://${DIRECTLINE_ENDPOINT_URI || "directline.botframework.com"}/v3/directline/tokens/generate`;
-
-// Initialize the web app instance,
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-app.use(cookieParser());
 
-let options = {};
-// uncomment the line below if you wish to allow only specific domains to embed this page as a frame
-//options = {setHeaders: (res, path, stat) => {res.set('Content-Security-Policy', 'frame-ancestors example.com')}};
-// Indicate which directory static resources
-// (e.g. stylesheets) should be served from.
-app.use(express.static(path.join(__dirname, "public"), options));
-// begin listening for requests.
-const port = process.env.PORT || 8080;
-const region = process.env.REGION || "Unknown";
+// Middleware to parse JSON data from POST requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(port, function() {
-    console.log("Express server listening on port " + port);
+// Initialize a variable to store the user's input
+let userInput = '';
+
+// Serve HTML page with a text input and a submit button
+app.get('/', (req, res) => {
+  res.send(`
+    <html>
+      <body>
+        <h1>Simple Node.js Web App</h1>
+        <form action="/update" method="post">
+          <input type="text" name="text" placeholder="Enter text" />
+          <button type="submit">Submit</button>
+        </form>
+        <p>User Input: ${userInput}</p>
+      </body>
+    </html>
+  `);
 });
 
-function isUserAuthenticated(){
-    // add here the logic to verify the user is authenticated
-    return true;
-}
-
-const appConfig = {
-    isHealthy : false,
-    options : {
-        method: 'POST',
-        headers: {
-            'Authorization': 'Bearer ' + WEBCHAT_SECRET
-        }
-    }
-};
-
-function healthResponse(res, statusCode, message) {
-    res.status(statusCode).send({
-        health: message,
-        region: region
-    });
-}
-function healthy(res) {
-    healthResponse(res, 200, "Ok");
-}
-
-function unhealthy(res) {
-    healthResponse(res, 503, "Unhealthy");
-}
-
-app.get('/health', async function(req, res){
-    if (!appConfig.isHealthy) {
-        try {
-            const fetchResponse = await fetch(directLineTokenEp, appConfig.options);
-            const parsedBody = await fetchResponse.json();
-            appConfig.isHealthy = true;
-            healthy(res);
-        }
-        catch (err) {
-            unhealthy(res);
-        }
-    }
-    else {
-        healthy(res);
-    }
+// Handle POST request to update the variable with user input
+app.post('/update', (req, res) => {
+  userInput = req.body.text;
+  res.redirect('/');
 });
 
-app.post('/chatBot',  async function(req, res) {
-    if (!isUserAuthenticated()) {
-        res.status(403).send();
-        return;
-    }
-    try {
-        const fetchResponse = await fetch(directLineTokenEp, appConfig.options);
-        const parsedBody = await fetchResponse.json();
-        var userid = req.query.userId || req.cookies.userid;
-        if (!userid) {
-            userid = crypto.randomBytes(4).toString('hex');
-            res.cookie("userid", userid, { sameSite: "none", secure: true, httpOnly: true, expires: new Date(new Date().setFullYear(new Date().getFullYear() + 1)) });
-        }
-
-        var response = {};
-        response['userId'] = userid;
-        response['userName'] = req.query.userName;
-        response['locale'] = req.query.locale;
-        response['connectorToken'] = parsedBody.token;
-
-        /*
-        //Add any additional attributes
-        response['optionalAttributes'] = {age: 33};
-        */
-
-        if (req.query.lat && req.query.long)  {
-            response['location'] = {lat: req.query.lat, long: req.query.long};
-        }
-        response['directLineURI'] = DIRECTLINE_ENDPOINT_URI;
-        const jwtToken = jwt.sign(response, APP_SECRET);
-        res.send(jwtToken);
-    }
-    catch (err) {
-        appConfig.isHealthy = false;
-        res.status(err.statusCode).send();
-        console.log("failed");
-    }
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
